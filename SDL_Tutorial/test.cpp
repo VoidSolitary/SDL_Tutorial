6360f1711d874f497cd771dac3128beb15684262
Jing Wang <cis.wangjing@gmail.com>
2016-10-29 09:40:17 +0100

    lesson 4 completed

--- SDL_Tutorial/test.cpp
+++ SDL_Tutorial/test.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 #include <SDL.h>
+#include <SDL_image.h>
 #include "cleanup.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const std::string RESOURCE_PATH = "res\\";
+const int TILE_SIZE = 40;
 
 /**
 * Log an SDL error with some error message to the output stream of our choice
@@ -22,7 +24,7 @@ void logSDLError(std::ostream &os, const std::string &msg) {
 * @param ren The renderer to load the texture onto
 * @return the loaded texture, or nullptr if something went wrong.
 */
-SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
+SDL_Texture* loadBmpTexture(const std::string &file, SDL_Renderer *ren) {
 	//Initialize to nullptr to avoid dangling pointer issues
 	SDL_Texture *texture = nullptr;
 	//Load the image
@@ -43,6 +45,40 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
 }
 
 /**
+* Loads an image into a texture on the rendering device
+* @param file The image file to load
+* @param ren The renderer to load the texture onto
+* @return the loaded texture, or nullptr if something went wrong.
+*/
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
+	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+	if (texture == nullptr) {
+		logSDLError(std::cout, "LoadTexture");
+	}
+	return texture;
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+* width and height
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param x The x coordinate to draw to
+* @param y The y coordinate to draw to
+* @param w The width of the texture to draw
+* @param h The height of the texture to draw
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h) {
+	//Setup the destination rectangle to be at the position we want
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	dst.w = w;
+	dst.h = h;
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+/**
 * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
 * the texture's width and height
 * @param tex The source texture we want to draw
@@ -51,13 +87,9 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
 * @param y The y coordinate to draw to
 */
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y) {
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	//Query the texture to get its width and height to use
-	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	SDL_RenderCopy(ren, tex, NULL, &dst);
+	int w, h;
+	SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+	renderTexture(tex, ren, x, y, w, h);
 }
 
 int main(int, char**) {
@@ -66,7 +98,13 @@ int main(int, char**) {
 		return 1;
 	}
 
-	SDL_Window *window = SDL_CreateWindow("Lesson 2", 100, 100, SCREEN_WIDTH,
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
+		logSDLError(std::cout, "IMG_Init");
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Window *window = SDL_CreateWindow("Lesson 3", 100, 100, SCREEN_WIDTH,
 		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
 		logSDLError(std::cout, "CreateWindow");
@@ -83,24 +121,29 @@ int main(int, char**) {
 		return 1;
 	}
 
-	SDL_Texture *background = loadTexture(RESOURCE_PATH + "background.bmp", renderer);
-	SDL_Texture *image = loadTexture(RESOURCE_PATH + "image.bmp", renderer);
-	if (background == nullptr || image == nullptr) {
-		cleanup(background, image, renderer, window);
+	SDL_Texture *image = loadTexture(RESOURCE_PATH + "lesson4.png", renderer);
+	if (image == nullptr) {
+		cleanup(image, renderer, window);
 		SDL_Quit();
 		return 1;
 	}
 
-	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i) {
-		SDL_RenderClear(renderer);
+	SDL_Event e;
+	bool quit = false;
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+			if (e.type == SDL_KEYDOWN) {
+				quit = true;
+			}
+			if (e.type == SDL_MOUSEBUTTONDOWN) {
+				quit = true;
+			}
+		}
 
-		int bW, bH;
-		SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
-		renderTexture(background, renderer, 0, 0);
-		renderTexture(background, renderer, bW, 0);
-		renderTexture(background, renderer, 0, bH);
-		renderTexture(background, renderer, bW, bH);
+		SDL_RenderClear(renderer);
 
 		int iW, iH;
 		SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
@@ -109,12 +152,9 @@ int main(int, char**) {
 		renderTexture(image, renderer, x, y);
 
 		SDL_RenderPresent(renderer);
-
-		//Take a quick break after all that hard work
-		SDL_Delay(1000);
 	}
 
-	cleanup(background, image, renderer, window);
+	cleanup(image, renderer, window);
 	SDL_Quit();
 
 	return 0;
